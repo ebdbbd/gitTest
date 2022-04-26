84cf2072d203461303adebdc8671a840fbdc60a2
ebdbbd <ebdbbd@qq.com>
2022-04-26 19:34:48 +0800

    first Committed

---
+++ src/main/java/com/atguigu/pojo/test.java
@@ -0,0 +1,15 @@
+package com.atguigu.pojo;
+
+/**
+ * @author :ebdbbd
+ * @declaration:
+ * @create 2022-04-26 16:00
+ */
+public class test {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello,git!");
+
+    }
+}
