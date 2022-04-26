e5fc98cde91293b0502024fd1bf7e70104f17340
ebdbbd <ebdbbd@qq.com>
2022-04-26 20:14:02 +0800

    master_2

--- src/main/java/com/atguigu/pojo/test.java
+++ src/main/java/com/atguigu/pojo/test.java
@@ -12,7 +12,7 @@ public class test {
         System.out.println("hello,git!");
         System.out.println("hello,git2");
         System.out.println("hello,git3");
-
         System.out.println("hello,git4");
+        System.out.println("master_2");
     }
 }
